9c77eeffca4d63c6691b4b1877bd6b3fdf3df5c0
Tony Aldridge <zaragopha@hotmail.com>
2015-01-05 13:55:56 +0000

    Corrected scrub position (horrendously) in tokenizer

--- src/window.c
+++ src/window.c
@@ -6,7 +6,7 @@
 short make_object_colors(short border_color, short text_color, short draw_mode, short fill_pattern, short fill_color) {
     return (border_color << 12) | (text_color << 8) | (draw_mode << 7) | (fill_pattern << 4) | fill_color;
 }
-
+//"llama llama llama llama llama llama\n"
 bool tokenize_multiline(char *buffer, const char *input, short max_width, short text_size, short *last_index) {
     short input_length = strlen(input);
     short buffer_position = 0;
@@ -18,32 +18,34 @@ bool tokenize_multiline(char *buffer, const char *input, short max_width, short 
 
     while (*last_index < input_length) {
         if (input[*last_index] == '\n') {
+            buffer[buffer_position] = 0;
+            buffer_position++;
             (*last_index)++;
             break;
         } else {
             buffer[buffer_position] = input[*last_index];
             buffer_position++;
+            //Make sure we always end our string!
+            buffer[buffer_position] = 0;
             (*last_index)++;
         }
     }
 
-    //Make sure we always end our string!
-    buffer[buffer_position] = 0;
-
     //If we need to wrap, we should do so now
     short width_char_difference = buffer_position - (max_width / text_size);
-    short inspected_char = 0;
+    short inspected_char_pos = 0;
     short original_last_index = *last_index;
+
     if (width_char_difference > 0) {
-        inspected_char = buffer_position - width_char_difference;
+        inspected_char_pos = buffer_position - width_char_difference;
         (*last_index) -= width_char_difference;
 
-        while (inspected_char > 0 && buffer[inspected_char] != ' ') {
-            inspected_char--;
+        while (inspected_char_pos > 0 && buffer[inspected_char_pos] != ' ') {
+            inspected_char_pos--;
             (*last_index)--;
         }
-        if (buffer[inspected_char] == ' ') {
-            buffer[inspected_char] = 0;
+        if (buffer[inspected_char_pos] == ' ') {
+            buffer[inspected_char_pos] = 0;
             (*last_index)++;
         } else {
             *last_index = original_last_index;
